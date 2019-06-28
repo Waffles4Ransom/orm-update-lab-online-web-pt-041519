fc30fe7b5323114c16bcb3d222c477725386f157
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-28 03:57:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade, :id
   
- def initialize(id = nil, name, grade)
+ def initialize(name, grade, id = nil)
   @name = name 
   @grade = grade 
   @id = id 
