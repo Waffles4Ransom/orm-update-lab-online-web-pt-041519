746305e9a98e55b8303e120dfe6f4277138cb5db
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-28 03:42:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,10 @@ class Student
 
      DB[:conn].execute(sql, self.name, self.grade)
      @ID = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-   end
-   end
+    end
+  end
+  
+  
+  
+  
  end
